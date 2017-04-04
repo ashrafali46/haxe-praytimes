03823484a80ecbfaf107beacba1a4406e8627e69
Mansour Djawadi <mansurjavadi@gmail.com>
2017-04-04 19:55:02 +0430

    added mid modes constants

---
+++ org/praytimes/constants/MidnightMode.hx
@@ -0,0 +1,14 @@
+package org.praytimes.constants;
+
+
+class MidnightMode
+{
+    /**Mid Sunset to Sunrise*/
+    public static inline var STANDARD : String = "Standard";
+    /**Mid Sunset to Fajr*/
+    public static inline var JAFARI : String = "Jafari";
+
+    public function new()
+    {
+    }
+}
