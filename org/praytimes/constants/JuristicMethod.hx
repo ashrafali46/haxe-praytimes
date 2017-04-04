f67b3088fb0c5d97a6d10c27377b6ff6c77b26e9
Mansour Djawadi <mansurjavadi@gmail.com>
2017-04-04 19:55:41 +0430

    added juristic constants

---
+++ org/praytimes/constants/JuristicMethod.hx
@@ -0,0 +1,14 @@
+package org.praytimes.constants;
+
+
+class JuristicMethod
+{
+    /**Shafi`i, Maliki, Ja`fari, Hanbali*/
+    public static inline var STANDARD : String = "Standard";
+    /**Hanafi*/
+    public static inline var HANAFI : String = "Hanafi";
+
+    public function new()
+    {
+    }
+}
